76a0c5d0171ae4435134cc6d72d069ae9aa4a838
zhongwq <1316628630@qq.com>
2018-12-24 20:41:54 +0800

    Fix bug

--- privileges.sql
+++ privileges.sql
@@ -1,5 +1,5 @@
 use mysql;
 select host, user from user;
 create user root identified by 'zsy2720a';
-grant all on docker_mysql.* to docker@'%' identified by 'zsy2720a' with grant option;
+grant all on blog_db.* to root@'%' identified by 'zsy2720a' with grant option;
 flush privileges;
